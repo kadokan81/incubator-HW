8d5be891db6c732d1d0ae6c00950ccd6fcb03250
kadokan81 <kadokan81@gmail.com>
2023-04-25 15:11:16 +0200

    describe MessageType ,MessagePropsType start styling messGE

---
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import s from './FriendMessage.module.css';
+
+import { MessagePropsType } from '../message/Message';
+import { MessageType } from '../HW1';
+
+// создать тип вместо any и отобразить приходящие данные
+type FrendMessagePropsType = {
+	message: MessageType;
+};
+
+const FriendMessage = ({ message }: FrendMessagePropsType) => {
+	const { id, user } = message;
+	return (
+		<div id={'hw1-friend-message-' + id} className={s.friendMessage}>
+			<div className={s.friendImageAndText}>
+				<img
+					id={'hw1-friend-avatar-' + id}
+					// создаёт студент
+					src={user.avatar}
+					//
+				/>
+				<div className={s.friendText}>
+					<div id={'hw1-friend-name-' + id} className={s.friendName}>
+						{/*создаёт студент*/}
+						{message.user.name}
+						{/**/}
+					</div>
+					<pre id={'hw1-friend-text-' + id} className={s.friendMessageText}>
+						{/*создаёт студент*/}
+						{message.message.text}
+						{/**/}
+					</pre>
+				</div>
+			</div>
+			<div id={'hw1-friend-time-' + id} className={s.friendTime}>
+				{/*создаёт студент*/}
+				{message.message.time}
+				{/**/}
+			</div>
+		</div>
+	);
+};
+
+export default FriendMessage;
