b3f04caf0d28c4291024b4e596fd759228219185
kadokan81 <kadokan81@gmail.com>
2023-06-05 21:21:44 +0200

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,60 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+	| { type: 'sort'; payload: 'up' | 'down' }
+	| { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+// Custom comparison function
+// function compare(a, b) {
+//     const nameA = a.name.toUpperCase(); // Convert names to uppercase for case-insensitive sorting
+//     const nameB = b.name.toUpperCase();
 
-            return state // need to fix
-        }
-        case 'check': {
+//     if (nameA < nameB) {
+//       return -1; // a should come before b in the sorted order
+//     } else if (nameA > nameB) {
+//       return 1; // a should come after b in the sorted order
+//     }
+//     return 0; // names are equal
+//   }
 
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => {
+	// need to fix any
+	switch (action.type) {
+		case 'sort': {
+			if (action.payload === 'up') {
+				return state.sort((a, b) => {
+					const nameA = a.name.toLowerCase();
+					const nameB = b.name.toLowerCase();
+
+					if (nameA < nameB) {
+						return -1; // a should come before b
+					} else if (nameA > nameB) {
+						return 1; // a should come after b
+					} else {
+						return 0; // names are equal
+					}
+				});
+			} else if (action.payload === 'down') {
+				return state.sort((a, b) => {
+					const nameA = a.name.toLowerCase();
+					const nameB = b.name.toLowerCase();
+
+					if (nameA > nameB) {
+						return -1; // a should come before b
+					} else if (nameA < nameB) {
+						return 1; // a should come after b
+					} else {
+						return 0; // names are equal
+					}
+				});
+			} else {
+				return state;
+			}
+		}
+		case 'check': {
+			return state.filter((user) => user.age > action.payload); // need to fix
+		}
+		default:
+			return state;
+	}
+};
